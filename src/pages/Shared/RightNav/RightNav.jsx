3439bcdd5b5831f35570c404d3b39f45c53982c6
sagorforu <freelancersagor18@gmail.com>
2023-04-28 20:39:25 +0600

    right side nav bar completed

--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -1,11 +1,56 @@
-import React from 'react';
+import React from "react";
+import { Button, ListGroup } from "react-bootstrap";
+import {
+  FaFacebookF,
+  FaGithub,
+  FaGoogle,
+  FaInstagram,
+  FaTwitter,
+} from "react-icons/fa";
+import QZone from "../Qzone/Qzone";
+import bg from "../../../assets/bg.png";
+import './RightNav.css'
 
 const RightNav = () => {
-    return (
-        <div>
-            <h2>Right Navbar</h2>
+  return (
+    <div>
+      <div>
+        <h4>Login</h4>
+        <Button className="mb-2" variant="outline-primary">
+          <FaGoogle /> Login with Google
+        </Button>
+        <Button variant="outline-secondary">
+          <FaGithub /> Login with Github
+        </Button>
+      </div>
+      <div>
+        <h4>Find Us On</h4>
+        <ListGroup>
+          <ListGroup.Item>
+            <FaFacebookF /> Facebook
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <FaTwitter /> Twitter
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <FaInstagram /> Instagram
+          </ListGroup.Item>
+        </ListGroup>
+      </div>
+      <QZone></QZone>
+      <div className="mt-4">
+        <img className="right-bg-img" src={bg} alt="" />
+        <div className="right-bg-text">
+          <h4>Create an Amazing Newspaper</h4>
+          <p>
+            Discover thousands of options, easy to customize layouts, one-click
+            to import demo and much more.
+          </p>
+          <Button variant="danger">Learn More</Button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default RightNav;
