49faea82a01fe08119eb137aaf88a131c4c268ad
sagorforu <freelancersagor18@gmail.com>
2023-05-01 20:18:52 +0600

    ratings added

--- src/pages/Home/NewsCard/NewsCard.jsx
+++ src/pages/Home/NewsCard/NewsCard.jsx
@@ -2,8 +2,15 @@ import moment from "moment";
 import React from "react";
 import { Button, Card, Image } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { FaEye, FaRegBookmark, FaRegStar, FaShareAlt, FaStar } from "react-icons/fa";
-import Rating from "react-rating";
+import {
+  FaEye,
+  FaRegBookmark,
+  FaRegStar,
+  FaShareAlt,
+  FaStar,
+} from "react-icons/fa";
+import { Rating } from "@smastrom/react-rating";
+import "@smastrom/react-rating/style.css";
 
 const NewsCard = ({ news }) => {
   const { title, details, _id, image_url, author, total_view, rating } = news;
@@ -43,15 +50,9 @@ const NewsCard = ({ news }) => {
           </Card.Text>
         </Card.Body>
         <Card.Footer className="text-muted d-flex">
-          <div className="flex-grow-1">
-            <Rating
-              placeholderRating={rating.number}
-              emptySymbol={<FaRegStar></FaRegStar>}
-              placeholderSymbol={<FaStar className="text-warning"></FaStar>}
-              fullSymbol={<FaStar></FaStar>}
-              readonly
-            ></Rating>
-            <span>{rating.number}</span>
+          <div className="flex-grow-1 d-flex align-items-center">
+            <Rating style={{ maxWidth: 100 }} value={rating.number} readOnly />
+            <span className="ms-2">{rating.number}</span>
           </div>
           <div>
             <FaEye></FaEye> {total_view}
